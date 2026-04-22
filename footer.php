3b0815a0f7419ee4ada651647cb1601ecef8b800
ksroka-stud <k.sroka@stud.abingdon-witney.ac.uk>
2026-04-22 15:10:18 +0100

    Changed footer to static

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <style>
-    footer{position: relative; bottom: 0; background-color: #111; color: white; text-align: center; padding: 18px; margin-top: 30px;}
+    footer{position: static; bottom: 0; background-color: #111; color: white; text-align: center; padding: 18px; margin-top: 30px;}
 </style>
 
 </body>
