9ef290e83fc930ce5ea3be15272a9621ccb7ad44
ksroka-stud <k.sroka@stud.abingdon-witney.ac.uk>
2026-04-24 16:19:53 +0100

    Changes for footer

--- header.php
+++ header.php
@@ -12,7 +12,8 @@ if (session_status() === PHP_SESSION_NONE) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <style>
-        body { font-family: Arial, sans-serif; margin: 0; padding: 0; line-height: 1.5; }
+        html, body{height: 100%;margin: 0;}
+        body { font-family: Arial, sans-serif; margin: 0; padding: 0; line-height: 1.5; display: flex; flex-direction: column; min-height: 100vh; background-color: #f4f4f4;}
         nav a{color: rgb(255, 255, 255);margin: 0 10px;text-decoration: none;font-weight: bold;}
         form { margin: 20px 0; padding: 20px; border: 1px solid #ccc; border-radius: 8px; }
         label { display: block; margin-top: 12px; font-weight: bold; }
