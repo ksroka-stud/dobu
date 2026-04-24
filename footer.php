d97ae639b231bc8fdf7546c1e87ed411ec03835a
ksroka-stud <k.sroka@stud.abingdon-witney.ac.uk>
2026-04-24 16:19:47 +0100

    Footer changes reverted

--- footer.php
+++ footer.php
@@ -1,6 +1,4 @@
 <style>
-    html, body{height: 100%;margin: 0;}
-    body { font-family: Arial, sans-serif; margin: 0; padding: 0; line-height: 1.5; display: flex; flex-direction: column; min-height: 100vh; background-color: #f4f4f4;}
     footer{background-color: #111; color: white; text-align: center; padding: 18px; margin-top: 30px;}
 </style>
 
