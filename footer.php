13905fcd72df1e9ee210f64bcf0ed6e6b8459d05
ksroka-stud <k.sroka@stud.abingdon-witney.ac.uk>
2026-04-27 11:46:50 +0100

    Cleanup unecessary variables

--- footer.php
+++ footer.php
@@ -1,7 +1,3 @@
-<style>
-    footer{background-color: #111; color: white; text-align: center; padding: 18px; margin-top: 30px;}
-</style>
-
 </body>
     <footer>
         <p>© 2026 DoBu Martial Arts | All Rights Reserved</p>
