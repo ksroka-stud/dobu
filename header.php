604986f97f1f9a1702849b4ae06a0a2792d88f05
ksroka-stud <k.sroka@stud.abingdon-witney.ac.uk>
2026-04-27 11:46:38 +0100

    Add .css

--- header.php
+++ header.php
@@ -5,6 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="css/style.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
