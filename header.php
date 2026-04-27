1be029ed77cc7247ea4990d65872775958beb646
ksroka-stud <k.sroka@stud.abingdon-witney.ac.uk>
2026-04-27 11:53:01 +0100

    Correct .css file name

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <title>DoBu Martial Arts - Membership</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header>
